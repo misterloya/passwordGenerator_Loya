31dfb9fcd08866e59873687e61071f74ae70b061
Issac Loya <misterloya@gmail.com>
2020-10-31 22:28:52 -0500

    final push with read me edits.

--- README.md
+++ README.md
@@ -3,8 +3,21 @@
 Links to access Git Hub & Live URL  
     https://misterloya.github.io/passwordGenerator_Loya/
 
-    
+
     https://github.com/misterloya/passwordGenerator_Loya
 
     This Project asks us to prompt a user with several options to generate a password that is based on input 
     from the user. 
+
+    I accessed ASCII characters to make things cleaner in the js, insead of creating arrays of uppercase, lowercase, and
+    special characters. I was able to call these values using fromCharCode.
+    http://www.asciitable.com/
+
+
+    The most interesting part about this homework, was the way in which i was tasked to compile if-statements that would satisfy the components of the generate button. 
+
+    I used html, css, and javascript to compile this project. 
+
+    The system involves defining variables to hold specific data that's compiled through prompts. the user's input , if made to satisfy all criteria, will be presented in a random password that includes all the 
+    input criteria. 
+
--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-// Assignment Code
+// #generate, is where the button is located in the html code, var generateBtn
+// begins to define where our user goes once he clicks on the button.
+// document.querySelector("#generate") pin points an ID using three steps, 
+// which then allows us to definte var generate Btn. 
 var generateBtn = document.querySelector("#generate");
 
-// Special characters for the function created
 
 
-// Write password to the #password input
+// create password function "writePassword" by calling upon generatePassword()
+// as defined in a var password. Then, we set var password, to the value in a
+// another var called passwordText. 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -13,7 +17,9 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// in order for our button to work, we create a constant called Generate Button 
+// then, add an event listener for a "click", whitewe can access the document, 
+// get elementByID, then acess the e
 
 const generateButton = document.getElementById("generateBtn")
 generateBtn.addEventListener("click", writePassword)
@@ -22,7 +28,7 @@ generateBtn.addEventListener("click", writePassword)
 
 
 
-// Prompts that come up after you click generate password
+// these prompts will gather information from the user through prompts
 function generatePassword(){
   var passwordLength = prompt ("Please enter the number of characters you want for your new password. It must be more than 8 but less than 128");
   
@@ -40,28 +46,27 @@ function generatePassword(){
     alert("Your password needs to be longer than 8 and shorter than 128");
     generatePassword();
 } 
-
+// so whatcha' whatcha' whatcha' want? 
   var numbers = confirm ("Do you want numbers in your password?");
   var upperCases = confirm ("Do you want uppercases in your password?");
   var lowerCases = confirm ("Do you want lowercases in your password?");
   var specialCharacters = confirm ("Do you want special characters in your password?");
-    
+    // if the user cancels out of everything, if the boolean is false,
+    // then we have to tell the user it's not possible to create a password
+    // and proceed to restart the site, hoping they change their 
     if (numbers === false && upperCases === false && lowerCases === false && specialCharacters === false){
      alert("Sorry we couldn't generate a password for you");
      generatePassword();
    }
 
-  // This is a minimum count for numbers, lowerCases, upperCases and specialCharacters 
+  // /
   var minimumCount = 0;
-
-  // Empty minimums for numbers, lowerCases and specialCharacters
-
-  var minimumNumbers;
+  var minimumNumbers = 0;
   var minimumLowerCases = "";
   var minimumUpperCases = "";
   var minimumSpecialCharacters ="";
 
-  // Generator functions**
+// functions to generate random numbers 
   var functionArray = {
     getNumbers: function(){
       return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
@@ -77,10 +82,9 @@ function generatePassword(){
     }
   };
 
-  // Checks to make sure user selected ok for all and uses empty minimums from above
 
-
-
+// if statements that measures whether or not user clicked ok or cancel. then it includes the corresn
+// corresponding information into a password
   if (numbers === true){
     minimumNumbers = functionArray.getNumbers();
     minimumCount++;
@@ -100,15 +104,15 @@ function generatePassword(){
   
 
   // Empty string variable for the loop below
-  var randomPasswordGenerated = [];
+  var randomPasswordGenerated = "";
 
   // Loop getting random characters
   for (let i = 0; i < (parseInt(passwordLength)); i++){
-    var randomNumberPicked = Math.floor(Math.random() * passwordLength);
+    var randomNumberPicked = Math.floor(Math.random() * 4);
     randomPasswordGenerated += randomNumberPicked;
   }
 
-  // To make sure characters are added to the password
+  // To make sure characters are added to the password /4
   randomPasswordGenerated += minimumNumbers;
   randomPasswordGenerated += minimumLowerCases;
   randomPasswordGenerated += minimumUpperCases;
